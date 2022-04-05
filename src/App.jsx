dcb8e6200aeafcdc1974a056d8225ae45d03dd9b
Carlos-Carsdfj <carsdfj@gmail.com>
2022-04-05 17:40:56 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import ResizeObserver from "react-resize-observer"
+import SearchBox from './components/SearchBox'
+import { Grid } from '@giphy/react-components'
+import { GiphyFetch } from '@giphy/js-fetch-api'
+function App() {
+  const [gifs, setGifs] = useState('dog')
+  const [width, setWidth] = useState(window.innerWidth)
+  const gf = new GiphyFetch('nokEIpoUxC3kQQW3gCN7rtLm219qmBeh')
+  console.log(gifs)
+  
+  const fetchGifs = (offset) => gf.search(gifs, { sort: 'relevant', lang: 'es',offset,limit: 10, type: 'gifs' })// gifs / stickers / text / videos
+  return (
+    <div className="  flex flex-col items-center justify-center w-full" >
+      <h1>Gify Search</h1>
+      <SearchBox  setGifs={setGifs} />
+      <hr className="my-6 "/>
+      <Grid
+        key={gifs}
+        fetchGifs={fetchGifs}
+        width={width}
+        columns={Math.floor((width /1000)*3)}
+        gutter={6}
+      />
+      <ResizeObserver
+        onResize={({ width }) => {
+          setWidth(width);
+        }}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SearchBox/index.jsx
@@ -0,0 +1,25 @@
+import  { useState } from 'react'
+import SvgComponent from './SvgComponent'
+export default function SearchBox({setGifs}) {
+  const  [ textToSearch, setTextToSearch ] = useState('')
+  const handleTextChanged = (ev) =>{
+    const text = ev.target.value
+    setTextToSearch(text)
+  }
+  const handleFindGif = ()=>{
+    setGifs(textToSearch)
+  }
+  return (
+    <div className='flex h-10 mt-2 gap-2 border-2 rounded-md border-blue-500 p-1'>
+      <input type="text"  className='form-control outline-none'
+        value={textToSearch}
+        onChange={handleTextChanged}
+      />
+      <button className='   px-5 flex  justify-center items-center bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out  align-center'
+        onClick={handleFindGif}
+      >
+        <SvgComponent/>
+      </button>
+    </div>
+  )
+}
