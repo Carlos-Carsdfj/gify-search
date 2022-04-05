c964214f1de8f27d402d8f5667d64597491936ae
Carlos-Carsdfj <carsdfj@gmail.com>
2022-04-05 19:01:52 -0400

    Fix perfomance

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   const fetchGifs = (offset) => gf.search(gifs, { sort: 'relevant', lang: 'es',offset,limit: 10, type: 'gifs' })// gifs / stickers / text / videos
   return (
     <div className="  flex flex-col items-center justify-center w-full" >
-      <h1>Gify Search</h1>
-      <SearchBox  setGifs={setGifs} />
+      <h1 className='font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600'>Gify Search</h1>
+      <SearchBox  setGifs={setGifs} titleSearch={'type and have fun'} />
       <hr className="my-6 "/>
       <Grid
         key={gifs}
--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,6 +1,6 @@
 import  { useState } from 'react'
 import SvgComponent from './SvgComponent'
-export default function SearchBox({setGifs}) {
+export default function SearchBox({setGifs, titleSearch ='search'}) {
   const  [ textToSearch, setTextToSearch ] = useState('')
   const handleTextChanged = (ev) =>{
     const text = ev.target.value
@@ -9,17 +9,25 @@ export default function SearchBox({setGifs}) {
   const handleFindGif = ()=>{
     setGifs(textToSearch)
   }
-  return (
+  return (<>
+    <label htmlFor='inputSearch'>
+    { titleSearch }
+  </label>
+
     <div className='flex h-10 mt-2 gap-2 border-2 rounded-md border-blue-500 p-1'>
-      <input type="text"  className='form-control outline-none'
+
+      <input id='inputSearch' type="text"  className='form-control outline-none'
         value={textToSearch}
         onChange={handleTextChanged}
       />
-      <button className='   px-5 flex  justify-center items-center bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out  align-center'
+      <button className='px-5 flex  justify-center items-center bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out  align-center'
+        aria-label="Center Align"
         onClick={handleFindGif}
       >
         <SvgComponent/>
       </button>
+
     </div>
+</>
   )
 }
